a10821ca10aa516515d14b5d59fc5d2b1d1c3960
AndrzejKnit <andrzej.knitter@gmail.com>
2019-02-08 11:48:17 +0100

    Lesson 52: Add, Update & Delete Logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -15,17 +15,48 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     // Subscripe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
     });
   }
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // Creat a new log
+      const newLog = {
+        id: this.genrateID(),
+        text: this.text,
+        date: new Date()
+      }
+      // Add console.log();
+      this.logService.addLog(newLog);
+    } else {
+      // Createlog to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+  }
 
+  genrateID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 }
